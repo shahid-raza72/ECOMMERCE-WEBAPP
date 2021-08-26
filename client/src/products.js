db498d0ae2cdc1d804a3a0edebe8e4d1e31fce2e
shahid <shahid@DESKTOP-K72K41M.(none)>
2021-08-26 23:02:46 +0530

    homescreen and product data created

---
+++ client/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'p1',
+    image: '/images/p1.jpg',
+    description:
+      'Slim Hand Bags',
+    brand: 'SheenIndia',
+    category: 'Bags',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'p2',
+    image: '/images/p2.jpg',
+    description:
+      'Slim Bags',
+    brand: 'SheenIndia',
+    category: 'Bags',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'p3',
+    image: '/images/p3.jpg',
+    description:
+      'Slim Bags',
+    brand: 'SheenIndia',
+    category: 'Bags',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'p4',
+    image: '/images/p4.jpg',
+    description:
+      'Slim Bags',
+    brand: 'SheenIndia',
+    category: 'Bags',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'p5',
+    image: '/images/p5.jpg',
+    description:
+      'Slim Bags',
+    brand: 'SheenIndia',
+    category: 'Bags',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'p6',
+    image: '/images/p6.jpg',
+    description:
+      'Slim Bags',
+    brand: 'SheenIndia',
+    category: 'Bags',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
